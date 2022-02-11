e8299c52dce147bf75f23e56567074a9d3ea85c8
muktar ahmed <muktar.ahmed13@yahoo.com>
2022-02-11 10:08:15 -0600

    Removing ignored files

--- model/Thought.js
+++ model/Thought.js
@@ -1,24 +1,35 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
+
+const reactionSchema = new Schema({
+  reactionId: {},
+  reactionBody: {},
+  username: {},
+  createdAt: {},
+});
 
 const ThoughtSchema = new Schema({
-
-    thoughtText:{
-        type: String,
+  thoughtText: {
+    type: String,
     required: true,
     // added char limit
-    },
-    createdAt:{
-        type: Date.now,
+  },
+  createdAt: {
+    type: Date.now,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: {
+    // Array of nested documents created with the reactionSchema
+  },
+});
 
-    },
-    username:{
-        type: String,
-        required: true,
-    },
-    reactions:{
-        // Array of nested documents created with the reactionSchema
-    }
+
+const Thought = model('Thought', ThoughtSchema)
+
+module.exports=Thought
 
 
-    
-})
+
+// fix gitignore
--- model/User.js
+++ model/User.js
@@ -1,26 +1,45 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const UserSchema = new Schema({
-   username:{
-    type: String,
-    unique: true,
-    required: true,
-    trim: true
-   },
-   email:{
-    type: String,
-    required: true,
-    unique: true,
-    // validate email
-   },
-thoughts:{
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      // validate email
+    },
+    thoughts: [
+        {
+            type:Schema.Types.ObjectId,
+            ref:'Thought'
+        }
+    ],
+   friends: [
+       {
+           type:Schema.Types.ObjectId,
+           ref:'User'
+       }
+   ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
 
-},
-friends:{
-    
-}
-    
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
 
+const User = model("User", UserSchema);
 
-
-  });
+module.exports = User;
